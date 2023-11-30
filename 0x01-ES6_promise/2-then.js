a6b1d8277ea446bca836fa4566371686debe01d4
Grace-ngigi <gracengigi859@gmail.com>
2023-11-30 08:50:36 +0300

    first then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise){
-    return new Promise((resolve, reject) => {
-        if (promise){
-            resolve({
-                status: 200,
-                body: "success"
-            },
-            console.log("Got a response from the API")
-            )
-        } else {
-            reject(new Error())
-        }
+    return promise
+    .then((data) => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      console.error('Got an error from the API');
+      return new Error();
     });
 }
