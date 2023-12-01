8d3bfc3cc439689c30dccc3c046acc6d3b1be0c2
Grace-ngigi <gracengigi859@gmail.com>
2023-12-01 13:05:49 +0300

    attempting to solve errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,29 +1,27 @@
 // Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js.
 
-// Write and export a function named handleProfileSignup. 
-// It should accept three arguments firstName (string), lastName (string), and fileName (string). 
-// The function should call the two other functions. 
+// Write and export a function named handleProfileSignup.
+// It should accept three arguments firstName (string), lastName (string), and fileName (string).
+// The function should call the two other functions.
 // When the promises are all settled it should return an array with the following structure:
 
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName){
-   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-   .then ((data) => {
-    console.log("data" + data)
-    return data.map((result) => ({
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => {
+      console.log(`data${data}`);
+      return data.map((result) => ({
         // console.log("result" + result)
         status: Promise.status,
-        value: result
-    }));
-   })
-   .catch ((error) => {
-    return [{
-        status: Promise.status,
-        value: error
-    }]
-   })
+        value: result,
+      }));
+    })
+    .catch((error) => [{
+      status: Promise.status,
+      value: error,
+    }]);
 }
 
 // [
