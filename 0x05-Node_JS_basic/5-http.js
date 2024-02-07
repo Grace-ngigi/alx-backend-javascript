363ea42047947f5766eb787d4d970d6324022a8f
Grace-ngigi <gracengigi859@gmail.com>
2024-02-07 05:44:07 +0300

    express server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,21 +1,51 @@
 const http = require('http');
+const fs = require('fs');
 
-const { countStudents } = require('./3-read_file_async');
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const students = {};
+        const lines = data.trim().split('\n');
+        lines.shift().split(',');
+        lines.forEach((line) => {
+          const values = line.split(',');
+          const firstname = values[0];
+          const field = values[3].trim();
+
+          if (!students[field]) {
+            students[field] = [];
+          }
+          students[field].push(firstname);
+        });
+        console.log(`Number of students: ${lines.length}`);
+        Object.keys(students).forEach((field) => {
+          console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+        });
+
+        resolve();
+      }
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
 
   if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
+    res.write('Hello Holberton School!\n');
+    res.end();
   } else if (req.url === '/students') {
-    countStudents('database.csv')
-      .then(() => {
-        console.log('Students count logged successfully.');
-        res.end('Students count logged successfully.\n');
-      })
-      .catch((error) => {
-        console.error(error.message);
-        res.end('Error: Cannot load the database.\n');
+    response.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+        const outString = output.slice(0, -1);
+        response.end(outString);
+      }).catch(() => {
+        response.statusCode = 404;
+        response.end('Cannot load the database');
       });
   } else {
     res.end('404 Not Found\n');
